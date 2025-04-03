d02b64463061eb05dec08c8ac9d02772511bc9ad
diegobq <diegobq@gmail.com>
2025-04-03 00:19:14 -0300

    default page

--- src/app/auth/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthForm from '@/components/AuthForm';
-
-export default function AuthPage() {
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <AuthForm />
-    </div>
-  );
-}
